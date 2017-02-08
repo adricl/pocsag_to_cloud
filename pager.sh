d2ce3302b8b7f2d1f1258871bba40f32817c1d36
adric <adric.landman@gmail.com>
2017-02-08 18:30:10 +1100

    Adding config file to load in startup scrips

--- pager.sh
+++ pager.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rtl_fm -A fast -f 148.637M -M fm -s 22050 -g 44.5 -l 305 | multimon-ng -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -f alpha /dev/stdin | dotnet run
+rtl_fm -A fast -f 148.637M -M fm -s 22050 -g 44.5 -l 305 | multimon-ng -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -f alpha /dev/stdin | dotnet run config.json
